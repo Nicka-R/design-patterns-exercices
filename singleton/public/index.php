393575d86b7f24ba9eb946de5e211756be7829b6
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-03-10 16:59:17 +0100

    ✨ implémentation du pattern singleton pour la classe Config

--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,7 +1,16 @@
 <?php
-require('../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
+use App\Config;
 
+// Retrieve an instance of Config
+$config = Config::getInstance();
 
-# TODO: Récuperer une instance de Config
-# Afficher une valeur contenu dans config.php
-# Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+echo 'Database Host: ' . $config->get('db')['host'] . PHP_EOL;
+
+$config2 = Config::getInstance();
+
+if ($config === $config2) {
+    echo 'Both instances are identical.' . PHP_EOL;
+} else {
+    echo 'Instances are different.' . PHP_EOL;
+}
